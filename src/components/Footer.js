7a3789b9909a519237c82232445fc5ae796a7905
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2022-11-17 13:43:56 +0600

    contact, banner, navbar, footer

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import { Container, Row, Col } from "react-bootstrap";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from '../assets/img/nav-icon4.png';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          {/* <MailchimpForm /> */}
+          <Col size={12} sm={6}>
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/eliushhimel/"><img src={navIcon1} alt="Icon" /></a>
+              <a target='_blank' rel="noreferrer" href="https://www.facebook.com/EliusHHimel/"><img src={navIcon2} alt="Icon" /></a>
+              <a target='_blank' rel="noreferrer" href="https://www.instagram.com/eliushhimel/"><img src={navIcon3} alt="Icon" /></a>
+              <a target='_blank' rel="noreferrer" href="https://www.github.com/eliushhimel/"><img src={navIcon4} alt="Icon" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
