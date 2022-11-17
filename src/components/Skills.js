9e8e073013794244e7cbdd43888005c8580b9c51
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2022-11-17 14:29:04 +0600

    skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,16 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import reactImg from "../assets/img/skills/React.png";
+import mongodb from "../assets/img/skills/mongodb.png";
+import expressjs from "../assets/img/skills/expressjs.png";
+import nodejs from "../assets/img/skills/node-js.png";
+import bootstrap from "../assets/img/skills/Bootstrap.png";
+import tailwindcss from "../assets/img/skills/tailwind-css.png";
+import materialui from "../assets/img/skills/material-ui.png";
+import html5 from "../assets/img/skills/html-5.png";
+import css3 from "../assets/img/skills/css-3.png";
+import javascript from "../assets/img/skills/js.png";
+
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
@@ -30,35 +36,59 @@ export const Skills = () => {
 
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                        </Carousel>
-                    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>I'm Experienced in the following languages and frameworks.</p>
+              <Carousel responsive={responsive} infinite={true} autoPlay={true} className="owl-carousel owl-theme skill-slider">
+                <div className="item">
+                  <img src={reactImg} alt="" />
+                  <h5>ReactJS</h5>
                 </div>
+                <div className="item">
+                  <img src={nodejs} alt="" />
+                  <h5>NodeJS</h5>
+                </div>
+                <div className="item">
+                  <img src={mongodb} alt="" />
+                  <h5>MongoDB</h5>
+                </div>
+                <div className="item">
+                  <img src={expressjs} alt="" />
+                  <h5>Express Js</h5>
+                </div>
+                <div className="item">
+                  <img src={html5} alt="" />
+                  <h5>HTML</h5>
+                </div>
+                <div className="item">
+                  <img src={css3} alt="" />
+                  <h5>CSS</h5>
+                </div>
+                <div className="item">
+                  <img src={javascript} alt="" />
+                  <h5>JavaScript</h5>
+                </div>
+                <div className="item">
+                  <img src={bootstrap} alt="" />
+                  <h5>Bootstrap</h5>
+                </div>
+                <div className="item">
+                  <img src={tailwindcss} alt="" />
+                  <h5>Tailwind CSS</h5>
+                </div>
+                <div className="item">
+                  <img src={materialui} alt="" />
+                  <h5>Material UI</h5>
+                </div>
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="" />
     </section>
   )
 }
